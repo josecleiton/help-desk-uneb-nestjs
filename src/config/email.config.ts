5838d3a10d4c534fd6b0bef2a12171a367e0f2c7
josecleiton <jcleitonbc@gmail.com>
2020-04-25 23:15:14 -0300

    fix: email config

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -1,4 +1,10 @@
-const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD } = process.env;
+const {
+  MAIL_FROM,
+  SMTP_HOST,
+  SMTP_PORT,
+  SMTP_USER,
+  SMTP_PASSWORD,
+} = process.env;
 
 export const emailConfig = {
   host: SMTP_HOST,
@@ -7,4 +13,4 @@ export const emailConfig = {
   auth: { user: SMTP_USER, pass: SMTP_PASSWORD },
 };
 
-export const emailFrom = `"EU" <jcleitonbc@gmail.com>`;
+export const emailFrom = MAIL_FROM;
