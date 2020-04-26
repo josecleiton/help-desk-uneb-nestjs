b1e00961492e09a2a3f313acf35472bee775ce2e
josecleiton <jcleitonbc@gmail.com>
2020-04-26 09:45:39 -0300

    feat: persist file uploaded

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import { MultipartFile } from './multipart-file.interface';
+import { PersistedFile } from './persisted-file.interface';
 
 @Controller('upload')
 export class UploadController {
@@ -13,7 +15,7 @@ export class UploadController {
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  upload(@UploadedFile() file) {
-    console.log(file);
+  upload(@UploadedFile() file: MultipartFile): PersistedFile {
+    return this.uploadService.save(file);
   }
 }
